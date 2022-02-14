7659ea08a56823ed23cde9d53594cc8262622558
Jamie Bunch <JamieBunch@ymail.com>
2022-02-14 15:55:44 -0500

    debugging example added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const sum = (a, b) => {
-    if(!a && b){
+    
+    if(!a || b){
+    console.log("Went inside if statement")
     return "invalid InPut"; 
 }
+    console.log("this is a", a, "this is b", b)
     return a + b;
 }
+
+console.log("The sum is:", sum(null, 6))
 
 module.exports = sum;
